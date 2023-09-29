d527291666c12511c905e0ce0ddcdaecc5741d60
Savindu Supun <savindusupunsathsara@gmail.com>
2023-09-29 20:50:00 +0530

    handle no URLs in project card 💫

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        // style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+      // style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
         <Image
           src={imgUrl}
@@ -19,22 +19,25 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           sizes="80%"
         />
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
-          <Link
+          {gitUrl !== "#" &&
+            <Link
             href={gitUrl}
             target="_blank"
             rel="noopener noreferrer"
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
-          <Link
-            href={previewUrl}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
-          >
-            <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
-          </Link>
+          </Link>}
+          {previewUrl !== "#" &&
+            <Link
+              href={previewUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            >
+              <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            </Link>
+          }
         </div>
       </div>
       <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
