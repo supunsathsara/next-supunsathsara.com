26845e45801ff61c6b84d11a0cd344b533b73965
Savindu Supun <savindusupunsathsara@gmail.com>
2023-10-23 17:29:54 +0530

    Add Holopin Board ✨

---
+++ src/app/components/HolopinBoard.jsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+const HolopinBoard = () => {
+    return (
+        <section className="justify-center">
+            <a href="https://www.holopin.io/@supunsathsara" target="_blank" rel="noopener noreferrer">
+                <Image
+                    src="https://holopin.me/supunsathsara"
+                    alt="@supunsathsara's Holopin board"
+                    className="rounded-xl cursor-pointer transform-gpu duration-200 mx-auto my-15 mb-20 w-[80%]"
+                    width={2428}
+                    height={764}
+                />
+            </a>
+        </section>
+    )
+}
+
+export default HolopinBoard;
