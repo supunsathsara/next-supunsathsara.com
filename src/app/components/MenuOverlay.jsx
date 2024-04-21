aeb4b05c8b0b93679a3a53a7cf726137b9d91912
Savindu Supun <contact@supunsathsara.com>
2024-04-22 03:07:03 +0530

    Add onClick event to close NavMenu  ✨

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import NavLink from "./NavLink";
 
-const MenuOverlay = ({ links }) => {
+const MenuOverlay = ({ links, onClose }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
         <li key={index}>
-          <NavLink href={link.path} title={link.title} />
+          <NavLink href={link.path} title={link.title} onClick={onClose} />
         </li>
       ))}
     </ul>
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -29,6 +29,8 @@ const navLinks = [
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
+  const closeNavbar = () => setNavbarOpen(false);
+
   return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-20 bg-opacity-100 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
@@ -67,7 +69,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      {navbarOpen ? <MenuOverlay links={navLinks} onClose={closeNavbar} /> : null}
     </nav>
   );
 };
