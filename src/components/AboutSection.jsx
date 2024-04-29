4b0db28cdb5cce9782645ee1d46ad57e216a6cc4
Savindu Supun <contact@supunsathsara.com>
2024-04-29 22:58:00 +0530

    add skill, react native and formatted with prettier ✨

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,13 +1,12 @@
 "use client";
 import React, { useTransition, useState, useEffect } from "react";
 import Image from "next/image";
-import { differenceInYears, isToday } from 'date-fns';
+import { differenceInYears, isToday } from "date-fns";
 import TabButton from "./TabButton";
 import certificates from "@/constants/certificates";
 import AboutImage from "@public/images/about-1.jpg";
 import AboutImage2 from "@public/images/about-2.jpg";
 import AboutImage3 from "@public/images/about-3.jpg";
-
 
 const TAB_DATA = [
   {
@@ -19,6 +18,7 @@ const TAB_DATA = [
         <li>Express Js</li>
         <li>Next Js</li>
         <li>React Js</li>
+        <li>React Native</li>
         <li>SQL</li>
         <li>NoSQL</li>
         <li>Tailwind CSS</li>
@@ -26,7 +26,6 @@ const TAB_DATA = [
         <li>Python</li>
         <li>Java</li>
       </ul>
-
     ),
   },
   {
@@ -40,7 +39,9 @@ const TAB_DATA = [
             <span>Higher National Diploma in Software Engineering</span>
           </div>
           <div className="text-sm text-gray-400">(Reading)</div>
-          <div className="text-sm text-gray-400">National Institute of Business Management</div>
+          <div className="text-sm text-gray-400">
+            National Institute of Business Management
+          </div>
           <div className="text-sm text-gray-400">2023</div>
         </li>
 
@@ -49,24 +50,29 @@ const TAB_DATA = [
           <div className="flex justify-between">
             <span>Diploma in Software Engineering</span>
           </div>
-          <div className="text-sm text-gray-300">Gold Medalist
+          <div className="text-sm text-gray-300">
+            Gold Medalist
             <span
               className="cursor-pointer"
               title="Recipient of the Gold Medal, awarded to the student with the highest academic achievement across all NIBM branches"
             >
-              <sup>{" "}!{" "}</sup>
+              <sup> ! </sup>
             </span>
-            / Scholarship Holder  <span
+            / Scholarship Holder{" "}
+            <span
               className="cursor-pointer animate-pulse"
               title="Recipient of the Gold Medal, awarded to the student with the highest academic achievement across all NIBM branches"
-            >🎖️</span></div>
+            >
+              🎖️
+            </span>
+          </div>
           <div className="text-sm text-gray-400">GPA: 4.00</div>
-          <div className="text-sm text-gray-400">National Institute of Business Management</div>
+          <div className="text-sm text-gray-400">
+            National Institute of Business Management
+          </div>
           <div className="text-sm text-gray-400">2022</div>
         </li>
-
       </ul>
-
     ),
   },
   {
@@ -83,21 +89,16 @@ const TAB_DATA = [
               rel="noopener noreferrer"
             >
               {certificate.name}
-            </a> by {certificate.provider}
+            </a>{" "}
+            by {certificate.provider}
           </li>
-        ))
-        }
-
-
+        ))}
       </ul>
-
-
     ),
   },
 ];
 
 const images = [AboutImage, AboutImage2, AboutImage3];
-
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
@@ -120,33 +121,52 @@ const AboutSection = () => {
   };
 
   useEffect(() => {
-    const birthDate = new Date('2002-08-26');
+    const birthDate = new Date("2002-08-26");
     const currentDate = new Date();
     const calculatedAge = differenceInYears(currentDate, birthDate);
 
     setAge(calculatedAge);
     setIsBirthday(isToday(birthDate));
-
   }, []);
 
   return (
     <section className="text-white z-10" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image className="shadow-lg z-10 rounded-sm" src={images[currentImageIndex]} width={500} alt="About image" />
+        <Image
+          className="shadow-lg z-10 rounded-sm"
+          src={images[currentImageIndex]}
+          width={500}
+          alt="About image"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg text-justify">
-            As a {age}-year-old {
-              isBirthday ? (
-                <span className="animate-pulse" title="it's my birthday today! 🥳">🎂</span>
-              ) : null
-            } Software Engineering undergraduate at NIBM Sri Lanka, I&apos;m a quick learner driven by a passion for web development.
+            As a {age}-year-old{" "}
+            {isBirthday ? (
+              <span
+                className="animate-pulse"
+                title="it's my birthday today! 🥳"
+              >
+                🎂
+              </span>
+            ) : null}{" "}
+            Software Engineering undergraduate at NIBM Sri Lanka, I&apos;m a
+            quick learner driven by a passion for web development.
+            <br />I currently work part-time as an{" "}
+            <span className="font-bold hover:bg-primary-500 bg-white text-[#030014] rounded-sm px-1">
+              Academic Tutor
+            </span>{" "}
+            at NIBM, where I am dedicated to providing educational support and
+            fostering a learning environment for students. Additionally, I am
+            involved as a{" "}
+            <span className="font-bold text-[#030014] bg-white hover:bg-[#FF6C37] hover:text-white rounded-sm px-1">
+              Postman Student Leader
+            </span>
+            , where I lead and mentor in the Postman student community.
             <br />
-            I currently work part-time as an {" "}
-            <span className="font-bold hover:bg-primary-500 bg-white text-[#030014] rounded-sm px-1">Academic Tutor</span> at NIBM, where I am dedicated to providing educational support and fostering a learning environment for students. Additionally, I am involved as a <span className="font-bold text-[#030014] bg-white hover:bg-[#FF6C37] hover:text-white rounded-sm px-1">Postman Student Leader</span>, where I lead and mentor in the Postman student community.
-            <br />
-            With a strong foundation in backend development, I bring responsive web design to life.
-            I am dedicated to crafting efficient solutions and continuously innovating in the tech world.
+            With a strong foundation in backend development, I bring responsive
+            web design to life. I am dedicated to crafting efficient solutions
+            and continuously innovating in the tech world.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
