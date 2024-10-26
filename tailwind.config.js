982b360164ad29eb556d0b483025b3c51fb7b80f
Savindu Supun <contact@supunsathsara.com>
2024-10-27 05:29:06 +0530

    feat: Add TracingBeam component for visual effect
    - Add a new TracingBeam component to create a visual effect on the certification page.
    
    chore: Update tailwind.config.js to include color variables
    
    - Update the tailwind.config.js file to include color variables for easier customization and theming.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 module.exports = {
   content: [
@@ -33,5 +36,18 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addVariablesForColors,
+  ],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
