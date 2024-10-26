1fc48abb41234f75a721d21aad43e5bea1a18ed5
Savindu Supun <contact@supunsathsara.com>
2024-10-27 05:29:06 +0530

    feat: Add TracingBeam component for visual effect
    - Add a new TracingBeam component to create a visual effect on the certification page.
    
    chore: Update tailwind.config.js to include color variables
    
    - Update the tailwind.config.js file to include color variables for easier customization and theming.

--- src/app/certifications/page.js
+++ src/app/certifications/page.js
@@ -1,24 +1,27 @@
 import CertificateItem from "@/components/CertificateItem";
 import GradualSpacing from "@/components/ui/GradualSpacing";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 import certificates from "@/constants/certificates";
 
 export default function CertificationPage() {
     return (
-        <div className="container min-h-screen mx-auto mt-24 my-8 relative z-5 text-center flex flex-col md:flex-col md:flex-wrap pb-12">
-            <div className="my-4 text-center mx-auto">
-                <GradualSpacing
-                    className="font-display my-4 text-center text-4xl font-bold  text-white md:text-7xl md:leading-[5rem]"
-                    text="Certifications"
-                />
-            </div>
+        <TracingBeam className="px-6 ">
+            <div className="container min-h-screen mx-auto mt-24 my-8 relative z-5 text-center flex flex-col md:flex-col md:flex-wrap pb-12">
+                <div className="my-4 text-center mx-auto">
+                    <GradualSpacing
+                        className="font-display my-4 text-center text-4xl font-bold  text-white md:text-7xl md:leading-[5rem]"
+                        text="Certifications"
+                    />
+                </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-7xl mx-auto md:px-8 w-full">
-                {certificates
-                    .sort((a, b) => b.id - a.id)
-                    .map((certificate, index) => (
-                        <CertificateItem key={index} certificate={certificate} index={index} />
-                    ))}
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-7xl mx-auto md:px-8 w-full">
+                    {certificates
+                        .sort((a, b) => b.id - a.id)
+                        .map((certificate, index) => (
+                            <CertificateItem key={index} certificate={certificate} index={index} />
+                        ))}
+                </div>
             </div>
-        </div>
+        </TracingBeam>
     );
 }
