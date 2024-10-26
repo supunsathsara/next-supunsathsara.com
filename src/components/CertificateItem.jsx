ec0ed7739301341d0ae89c892916746ef1611c76
Savindu Supun <contact@supunsathsara.com>
2024-10-27 04:46:06 +0530

    refactor: Update certificate page layout and improve image display
    - Refactored the CertificationPage component to use a grid layout for better responsiveness.
    - Updated the CertificateItem component to include image display and hover effects.
    - Sorted the certificates in reverse order by ID.

--- src/components/CertificateItem.jsx
+++ src/components/CertificateItem.jsx
@@ -1,30 +1,106 @@
-'use client'
+"use client";
 
+import { ArrowsPointingOutIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 const CertificateItem = ({ certificate, index }) => {
-    return (
-        <motion.li 
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8, delay: index * 0.2 }}
-            className="py-4 border-b border-[#33353F] shadow-[#2A0E61]/50"
-        >
-            <h3 className="text-xl font-semibold mb-2">
-                {certificate.name}
-                <span className="text-xl my-0 text-[#ADB7BE] ml-1"> by {certificate.provider}</span>
-            </h3>
+  const [isHovered, setIsHovered] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
-            <a
-                href={certificate.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#ADB7BE] hover:text-blue-600 hover:underline"
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.1 }}
+    >
+      <motion.div
+        className="rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-50 w-full transition-all duration-300 ease-out"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => setIsFullScreen(true)}
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+      >
+        <Image
+          src={`/certificates/${certificate.image}`}
+          alt={certificate.name}
+          width={500}
+          height={500}
+          className="object-cover absolute inset-0"
+        />
+
+        <div
+          className={`absolute inset-0 bg-black/50 flex items-end py-8 px-4 transition-opacity duration-300 ${
+            isHovered ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <div className="text-xl md:text-2xl font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-200">
+            {certificate.name}
+          </div>
+        </div>
+
+        {isHovered && (
+          <div className="absolute top-4 right-4 text-white">
+            <ArrowsPointingOutIcon className="h-6 w-6" />
+          </div>
+        )}
+      </motion.div>
+
+      <div className="mt-4 text-center">
+        <div className="text-xl md:text-2xl font-medium text-gray-900 dark:text-gray-100">
+          {certificate.name}
+        </div>
+        <div className="text-md md:text-lg text-gray-700 dark:text-gray-300">
+          {certificate.provider}
+        </div>
+      </div>
+
+      {isFullScreen && (
+        <motion.div
+          className="fixed inset-0 bg-black bg-opacity-75 flex flex-col items-center justify-center z-50"
+          onClick={() => setIsFullScreen(false)}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <motion.div
+            className="relative w-full h-3/4 md:w-3/4 flex flex-col items-center justify-center"
+            initial={{ scale: 0.8 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0.8 }}
+          >
+            <Image
+              src={`/certificates/${certificate.image}`}
+              alt={certificate.name}
+              width={1000}
+              height={1000}
+              className="object-contain"
+            />
+
+            <div
+              className="absolute top-4 right-4 text-white text-2xl cursor-pointer"
+              onClick={() => setIsFullScreen(false)}
             >
-                View Certificate
+              &times;
+            </div>
+          </motion.div>
+          <div className="w-full text-white text-lg text-center pt-4 bg-black bg-opacity-75 p-4">
+            <p>{certificate.provider}</p>
+            <a
+              href={certificate.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              View Certificate
             </a>
-        </motion.li>
-    );
+          </div>
+        </motion.div>
+      )}
+    </motion.div>
+  );
 };
 
 export default CertificateItem;
